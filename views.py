d6654c7469dca6cd84ea7d01d9a68c11046dcbec
GODWILL BINFA <111583356+Godwillgithub@users.noreply.github.com>
2022-11-25 16:40:15 +0100

    Add files via upload

--- views.py
+++ views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.def store(request):
+# Create your views here.
 
 def store(request):
-	context = {}
+	products = Product.objects.all()
+	context = {'products':products}
 	return render(request, 'store/store.html', context)
 
 def cart(request):
-	context = {}
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitems_set.all() 
+	else:
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items': 0 }
+
+	context = {'items': items, 'order': order}
 	return render(request, 'store/cart.html', context)
 
 def checkout(request):
